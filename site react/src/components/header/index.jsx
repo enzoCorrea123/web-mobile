485572ec9e581784c26a9fd1d3e76c3d202403c4
Enzo322 <enzorufino1508@gmail.com>
2023-10-14 16:57:49 -0300

    colocando fontawesome

--- site react/src/components/header/index.jsx
+++ site react/src/components/header/index.jsx
@@ -1,20 +1,29 @@
 import './header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 const headerSite = ()=>{
     return(
-        <div className='bg-black'> 
-            <img src="./public/logo.png" alt="logo"  />
-            <h1 className="text-4xl font-bold underline">Furniro</h1>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Shop</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-            <img src="./public/carinha.png" alt="user" />
-            <img src="./public/lupa.png" alt="lupa" />
-            <img src="./public/coracao.png" alt="coracao" />
-            <img src="./public/carrinho.png" alt="carrinho" />
-        </div>
+    <header className='flex items-center justify-between w-full h-24 py-7 pl-14 pr-24 '>
+    <div className='flex items-center gap-2'>
+        <img src="./public/logo.png" alt="logo"  />
+        <h1 className='text-4xl font-bold font-montserrat'>Furniro</h1>
+    </div>
+    <nav className='flex items-center justify-between w-96'>
+        <a className='text-base font-medium' >Home</a>
+        <a className='text-base font-medium' >Shop</a>
+        <a className='text-base font-medium' >About</a>
+        <a className='text-base font-medium' >Contact</a>
+    </nav>
+    <div className="flex items-center justify-between w-60">
+        <FontAwesomeIcon icon={faUser} size='xl' />
+        <FontAwesomeIcon icon={faMagnifyingGlass} size='xl' />
+        <FontAwesomeIcon icon={faHeart} size='xl' />
+        <FontAwesomeIcon icon={faCartShopping} size='xl' />
+    </div>
+</header>
         
     )
     
--- site react/tailwind.config.js
+++ site react/tailwind.config.js
@@ -3,9 +3,34 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      height: {
+        '440px': '440px',
+        '700px': '700px',
+      },
+      backgroundImage: {
+        'banner': "url('./public/images/banner.png')",
+      },
+      colors: {
+        'primary': '#B88E2F',
+        'secondary': '#FFF3E3',
+        'lightyellow': '#FAF3EA',
+        'lightlightyellow': '#FCF8F3',
+        'gray': '#F4F5F7',
+        'gray-font': '#898989',
+        'lightgray-font': '#B0B0B0',
+      },
+      letterSpacing: {
+        '3px': '3px',
+      },
+    },
   },
   plugins: [
   ],
